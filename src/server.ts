bbbdc02cd79707fe4cc0d1fec130d8fdab08da66
Roméo <giorgio.romeo95@gmail.com>
2021-11-01 13:50:56 +0100

    Add typescript dependencies and architecture

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express,{ Request, Response } from "express";
+export default class Server {
+
+    readonly port: number;
+    constructor (port:number){
+        this.port = port;
+    }
+    start(){
+        const app = express();
+        app.get('/', function(req:Request, res:Response){
+            res.send("Hello world");
+        })
+        app.listen(this.port, () =>{
+            console.log(`Server started on port ${this.port}`);
+        })
+    }
+}
