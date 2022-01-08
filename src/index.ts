e8efb28a768dbb6a98443082909c68d835635264
Roméo <giorgio.romeo95@gmail.com>
2022-01-08 18:25:33 +0100

    fix : MacOs don't allow localhost but 127.0.0.1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import mongoose from 'mongoose';
 const server = new Server(5500);
 server.start();
 
-mongoose.connect("mongodb://localhost:27017/liv-sig-gen-api", 
+mongoose.connect("mongodb://127.0.0.1:27017/liv-sig-gen-api", 
 (err)=>{
     if(!err)
         console.log("MongoDB connected");
