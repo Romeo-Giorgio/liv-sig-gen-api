7779e8e733a4de36f0718c90a3f1ea3b55b041d3
Roméo <giorgio.romeo95@gmail.com>
2022-04-21 22:22:20 +0200

    fix: add race's id even if mongodb have _id

--- src/models/racesModel.ts
+++ src/models/racesModel.ts
@@ -4,6 +4,7 @@ import { Folder } from "./foldersModel";
 
 //********** Types **********//
 export interface Race {
+  id: string;
   name: string;
   description?: string;
   folder?: Folder;
@@ -12,6 +13,7 @@ export interface Race {
 
 //********** Model **********//
 const racesSchema = new Schema<Race>({
+  id: { type: String, required: true },
   name: { type: String, required: true },
   description: { type: String, required: false },
   folder: { type: Schema.Types.ObjectId, ref: "Folder" },
